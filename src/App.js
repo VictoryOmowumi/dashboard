2490ff54cb7280f1d6adf607c4ea33d8baf9be03
VictoryOmowumi <victorybalogun@gmail.com>
2023-01-29 22:01:08 +0100

    first major commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useContext}  from 'react';
+import Home from './pages/home page/Home';
+import Login from './pages/login page/Login';
+import Single from './pages/single page/Single';
+import List from './pages/list page/List';
+import New from './pages/new page/New';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { productInputs, userInputs } from './formSource';
+import './style/Dark.css';
+import { DarkModeContext } from './context/darkModeContext';
 
 function App() {
+  const {darkMode} = useContext(DarkModeContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkMode ? "app dark" : "app"}>
+      <Router>
+        <Routes>
+        <Route path="/">
+            <Route index element={<Login />} />
+            <Route path='home' element={<Home  />}  />
+            {/* <Route path="login" element={<Login />} /> */}
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=':userId' element={<Single />} />
+              <Route path='new' element={<New inputs={userInputs} title="Add New User" buttonText="Send" />} />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=':productId' element={<Single />} />
+              <Route path='new' element={<New inputs={productInputs} title="Add New Item" buttonText="Add"/>} />
+            </Route>
+        </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
